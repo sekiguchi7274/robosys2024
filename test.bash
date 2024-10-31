b07cba7f29fb53919c3729153e3f65ad112e9cc2
sekiguchi <cyberakouxiang7274@gmail.com>
2024-10-31 12:46:09 +0900

    test.bash add

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash  
 
 ng () {
 	echo ${1}行目が違う
@@ -7,8 +7,18 @@ ng () {
 
 res=0
 
+### NORMAL input ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE input ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 exit "$res"
